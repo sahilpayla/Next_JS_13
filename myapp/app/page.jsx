d2efe43d9f157b17d6f5c401427bb4847c58912c
Shraddhanand <1shraddhanand@gmail.com>
2023-02-14 21:27:19 +0530

    next js folder structure

---
+++ myapp/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+   <div className="container">
+      Hello it is my first day using next js
+   </div>
+  )
+}
+
+export default page
